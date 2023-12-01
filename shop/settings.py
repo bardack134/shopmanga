59ffbd0b97cea0bf35831eadd687e542355481ae
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2023-12-02 07:08:11 +0900

    settings.py borrar lineas raras que salieron de re

--- shop/settings.py
+++ shop/settings.py
@@ -50,11 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-<<<<<<< HEAD
     "whitenoise.middleware.WhiteNoiseMiddleware",  #newly added
-=======
-    'whitenoise.middleware.WhiteNoiseMiddleware',
->>>>>>> 59952516634453172fb43dbab070acfb7bc61afd
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,13 +139,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
+
 STATIC_ROOT = BASE_DIR / "staticfiles" # represents the folder that will contain all staticfiles in production.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-=======
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
->>>>>>> 59952516634453172fb43dbab070acfb7bc61afd
+
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
